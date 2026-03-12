5ebd23df5aad41a07f47038f8eb582b1e99b8466
vivekbhogadi369-glitch <vivekbhogadi369@gmail.com>
2026-03-12 19:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ If PYQs are not available say:
 
 if __name__ == "__main__":
     
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
